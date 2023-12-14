88204560fd32c0192a82fb06bf2822f76b1a9963
Glalyyy <gamcth10a2.c3truonglongtay@gmail.com>
2023-12-14 12:30:13 +0700

    1.3.0.0

---
+++ Layers_cake/Layers_cake/Controllers/CartController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Layers_cake.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Thongtin()
+        {
+            return View();
+        }
+        public ActionResult Payment()
+        {
+            return View();
+        }
+        public ActionResult Thankyou()
+        {
+            return View();
+        }
+    }
+}
